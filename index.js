674354e12acc8a434f1872b3164ed744fa1047a3
Chris Williams <cjwolliams@gmail.com>
2022-09-06 10:58:22 -0700

    commit tree e2a16875258b71d192cd12bfd9dc1d459b1b6578

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+//This is really cool!
+//DOMContentLoaded
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+*/
+/*
+  function handleToDo(todo){
+    todo = "This is really cool!"
+    p.textContent = `${todo}`
+    document.querySelector().append(p)
+  }
+  */
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = 'This is really cool!';
+  });
+
+
+  //document.querySelector().textContent = 'This is really cool!'
